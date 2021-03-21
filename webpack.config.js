4d71bb62924dce4488db2dc109f69bde1642c769
rybakouvlad <rybakou.vladik@gmail.com>
2021-03-21 22:43:46 +0300

    feat: update webpack config and change title

--- webpack.config.js
+++ webpack.config.js
@@ -44,30 +44,30 @@ module.exports = (env) => {
     img: {
       test: /\.(gif|png|jpe?g)$/i,
       use: [
-        // 'file-loader',
-        // {
-        //   loader: 'image-webpack-loader',
-        //   options: {
-        //     mozjpeg: {
-        //       progressive: true,
-        //     },
-        //     // optipng.enabled: false will disable optipng
-        //     optipng: {
-        //       enabled: false,
-        //     },
-        //     pngquant: {
-        //       quality: [0.65, 0.9],
-        //       speed: 4,
-        //     },
-        //     gifsicle: {
-        //       interlaced: false,
-        //     },
-        //     // the webp option will enable WEBP
-        //     webp: {
-        //       quality: 75,
-        //     },
-        //   },
-        // },
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+            },
+            // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [0.65, 0.9],
+              speed: 4,
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            // the webp option will enable WEBP
+            webp: {
+              quality: 75,
+            },
+          },
+        },
         {
           loader: 'url-loader',
           options: {
