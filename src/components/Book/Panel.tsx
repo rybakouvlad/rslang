d417b941caec9f7c864e76c9c6c7ee303da66703
Dima Kukhta <dima.kukhta.2001@mail.ru>
2021-03-25 18:12:07 +0300

    feat: style for Panel

--- src/components/Book/Panel.tsx
+++ src/components/Book/Panel.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { changeGroup } from '../../store/actions/book';
 import { useTypeSelector } from '../../hooks/useTypesSelector';
+import { Button, ButtonGroup } from 'react-bootstrap';
 
 export const Panel: React.FC = () => {
   const dispatch = useDispatch();
@@ -12,88 +13,25 @@ export const Panel: React.FC = () => {
   };
 
   return (
-    <div
-      className="btn-group"
-      role="group"
-      aria-label="Basic radio toggle button group"
-      onChange={(e) => handlerRadioButton(e)}
-    >
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio1"
-        value="0"
-        autoComplete="off"
-        checked={group === 0}
-      />
-      <label className="btn btn-outline-primary" htmlFor="btnradio1">
+    <ButtonGroup aria-label="buttons" onClick={(e: any) => handlerRadioButton(e)}>
+      <Button value="0" active={group === 0}>
         1
-      </label>
-
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio2"
-        value="1"
-        autoComplete="off"
-        checked={group === 1}
-      />
-      <label className="btn btn-outline-primary" htmlFor="btnradio2">
+      </Button>
+      <Button value="1" active={group === 1}>
         2
-      </label>
-
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio3"
-        value="2"
-        autoComplete="off"
-        checked={group === 2}
-      />
-      <label className="btn btn-outline-primary" htmlFor="btnradio3">
+      </Button>
+      <Button value="2" active={group === 2}>
         3
-      </label>
-
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio4"
-        value="3"
-        autoComplete="off"
-        checked={group === 3}
-      />
-      <label className="btn btn-outline-primary" htmlFor="btnradio4">
+      </Button>
+      <Button value="3" active={group === 3}>
         4
-      </label>
-
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio5"
-        value="4"
-        autoComplete="off"
-        checked={group === 4}
-      />
-      <label className="btn btn-outline-primary" htmlFor="btnradio5">
+      </Button>
+      <Button value="4" active={group === 4}>
         5
-      </label>
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio6"
-        value="5"
-        autoComplete="off"
-        checked={group === 5}
-      />
-      <label className="btn btn-outline-primary" htmlFor="btnradio6">
+      </Button>
+      <Button value="5" active={group === 5}>
         6
-      </label>
-    </div>
+      </Button>
+    </ButtonGroup>
   );
 };
