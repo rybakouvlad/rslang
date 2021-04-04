2a25cecf8e1eb3bc405b81d4204db1b38916d630
Dima Kukhta <dima.kukhta.2001@mail.ru>
2021-04-04 20:50:50 +0300

    feat: chart js

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -6,7 +6,7 @@ export const Statistics: React.FC = () => {
   return (
     <div className="statistics-container">
       <BarChart data={[3, 2, 3, 2, 3]} labels={['24.03', '25.03', '26.03', '27.03', '28.03']} />
-      <LineChart data={[3, 2, 3, 4, 3]} labels={['24.03', '25.03', '26.03', '27.03', '28.03']} />
+      <LineChart data={[1, 3, 3, 3, 3]} labels={['24.03', '25.03', '26.03', '27.03', '28.03']} />
     </div>
   );
 };
