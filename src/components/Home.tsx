587e8f51f123f5d65f0660498661f08fa240089f
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-04-13 21:10:36 +0300

    feat: add simple page layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,30 +7,42 @@ export const Home: React.FC = () => {
 
   return (
     <main className="main_home__wrapper">
-      <a
-        className="home_title"
-        href="https://github.com/rolling-scopes-school/tasks/blob/master/tasks/react/react-rslang.md"
-        target="_blank"
-        rel="noreferrer"
-        style={{ textDecoration: 'none' }}
-      >
-        <h1>RS LANG</h1>
-        <div></div>
-      </a>
       <Container>
-        <div className="video_container">
-          <Video src={src}></Video>
-        </div>
-        <a
-          className="home_title"
-          href="https://github.com/rybakouvlad/rslang"
-          target="_blank"
-          rel="noreferrer"
-          style={{ textDecoration: 'none' }}
-        >
-          <h4>Репозиторий проекта</h4>
-          <div></div>
-        </a>
+        <Container>
+          <a
+            className="home_title"
+            href="https://github.com/rolling-scopes-school/tasks/blob/master/tasks/react/react-rslang.md"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: 'none' }}
+          >
+            <h1>RS LANG</h1>
+          </a>
+        </Container>
+        <Container>
+          <h4>
+            RS LANG поможет вам в изучении английского языка. Основой лёгкого изучения является игровой подход, который
+            обеспечит лучшую запоминаемость и быстрое усвоение новых слов.
+          </h4>
+        </Container>
+      </Container>
+      <Container>
+        <Container>
+          <div className="video_container">
+            <Video src={src}></Video>
+          </div>
+        </Container>
+        <Container>
+          <a
+            className="home_title"
+            href="https://github.com/rybakouvlad/rslang"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: 'none' }}
+          >
+            <h4>Репозиторий проекта: https://github.com/rybakouvlad/rslang</h4>
+          </a>
+        </Container>
       </Container>
     </main>
   );
