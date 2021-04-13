adfef4bf57fb21cbf44e0ccf13f7d074713e361e
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-04-13 23:42:19 +0300

    style: add and fix styles at home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Video } from './video/Video';
+import enPng from '../assets/img/en1.svg';
 
 export const Home: React.FC = () => {
   const src = 'https://www.youtube.com/watch?v=BuaZF0vItXI'; // video source
@@ -20,16 +21,28 @@ export const Home: React.FC = () => {
           </a>
         </Container>
         <Container>
-          <h4>
+          <p className="home_text">
             RS LANG поможет вам в изучении английского языка. Основой лёгкого изучения является игровой подход, который
             обеспечит лучшую запоминаемость и быстрое усвоение новых слов.
-          </h4>
+          </p>
         </Container>
       </Container>
       <Container>
         <Container>
           <div className="video_container">
             <Video src={src}></Video>
+            <div className="english_png">
+              <a
+                className="english_png__link"
+                target="_blank"
+                style={{ textDecoration: 'none' }}
+                href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D1%80%D0%B0%D0%BD,_%D0%B3%D0%B4%D0%B5_%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9_%D1%8F%D0%B2%D0%BB%D1%8F%D0%B5%D1%82%D1%81%D1%8F_%D0%BE%D1%84%D0%B8%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC_%D1%8F%D0%B7%D1%8B%D0%BA%D0%BE%D0%BC"
+                rel="noreferrer"
+              >
+                <img src={enPng} alt="En" />
+                <h6>Learn once, Speak anywhere!</h6>
+              </a>
+            </div>
           </div>
         </Container>
         <Container>
@@ -40,7 +53,7 @@ export const Home: React.FC = () => {
             rel="noreferrer"
             style={{ textDecoration: 'none' }}
           >
-            <h4>Репозиторий проекта: https://github.com/rybakouvlad/rslang</h4>
+            <p className="home_text">Репозиторий проекта: https://github.com/rybakouvlad/rslang</p>
           </a>
         </Container>
       </Container>
--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -8,11 +8,17 @@ type videoType = {
 export const Video: React.FC<videoType> = ({ src }: videoType) => {
   const [videoWidth, setVideoWidth] = useState(600);
   const [videoHeight, setVideoHeight] = useState(320);
+  const widthWindow = window.innerWidth;
+
   useEffect(() => {
+    if (widthWindow < 920) {
+      setVideoWidth(260);
+      setVideoHeight(180);
+    }
     const changeWidthSize = (event: any) => {
       if (event.target.innerWidth < 910) {
-        setVideoWidth(320);
-        setVideoHeight(200);
+        setVideoWidth(260);
+        setVideoHeight(180);
       }
       if (event.target.innerWidth > 920) {
         setVideoWidth(600);
