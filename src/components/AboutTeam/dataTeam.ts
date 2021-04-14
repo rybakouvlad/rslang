b5fd5c2113f90a777eaac406a7cbc0b163edb89b
Dima Kukhta <dima.kukhta.2001@mail.ru>
2021-04-14 20:13:03 +0300

    fix: index mistake

--- src/components/AboutTeam/dataTeam.ts
+++ src/components/AboutTeam/dataTeam.ts
@@ -50,7 +50,7 @@ export const data: Array<IDev> = [
   },
   {
     id: 5,
-    avatar: 'https://i.imgur.com/5xC76Sq.jpg',
+    avatar: 'https://i.imgur.com/A4Z0ztC.jpg',
     name: 'Dima',
     position: 'Software Enginer',
     discription: `Реализовал заголовок главной страницы, уведомление и его функциональность, логику промо страницы «начать
--- src/components/Book/Panel.tsx
+++ src/components/Book/Panel.tsx
@@ -42,61 +42,13 @@ export const Panel: React.FC = () => {
         value={element.index}
         active={group === element.index}
       >
-        {element.index}
+        {element.index + 1}
       </Button>
     );
   });
 
   return (
     <ButtonGroup aria-label="buttons" onClick={(e: any) => handlerRadioButton(e)}>
-      {/*<Button
-        style={{ backgroundColor: colors[0], borderColor: colors[0] }}
-        variant="outline-primary"
-        value="0"
-        active={group === 0}
-      >
-        1
-      </Button>
-      <Button
-        style={{ backgroundColor: colors[1], borderColor: colors[1] }}
-        variant="outline-primary"
-        value="1"
-        active={group === 1}
-      >
-        2
-      </Button>
-      <Button
-        style={{ backgroundColor: colors[2], borderColor: colors[2] }}
-        variant="outline-primary"
-        value="2"
-        active={group === 2}
-      >
-        3
-      </Button>
-      <Button
-        style={{ backgroundColor: colors[3], borderColor: colors[3] }}
-        variant="outline-primary"
-        value="3"
-        active={group === 3}
-      >
-        4
-      </Button>
-      <Button
-        style={{ backgroundColor: colors[4], borderColor: colors[4] }}
-        variant="outline-primary"
-        value="4"
-        active={group === 4}
-      >
-        5
-      </Button>
-      <Button
-        style={{ backgroundColor: colors[5], borderColor: colors[5] }}
-        variant="outline-primary"
-        value="5"
-        active={group === 5}
-      >
-        6
-      </Button>*/}
       {buttonsMap}
     </ButtonGroup>
   );
