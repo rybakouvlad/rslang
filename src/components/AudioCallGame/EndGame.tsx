7562482fcc07e9c4222dee9378a8ad92ba2b9b06
Tatsiana Novik <Tatsiana_Novik@epam.com>
2021-04-14 23:03:13 +0300

    feat: add styles to end game

--- src/components/AudioCallGame/EndGame.tsx
+++ src/components/AudioCallGame/EndGame.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { useAudioGame } from './audioGame.hook';
+// import { Button } from 'react-bootstrap';
+// import { useAudioGame } from './audioGame.hook';
 import { IResults } from './Game';
 interface iProps {
   results: IResults;
 }
 export const EndGame: React.FC<iProps> = (props: iProps) => {
-  const { repeatGame } = useAudioGame();
+  // const { repeatGame } = useAudioGame();
   return (
     <div className="audiocall-end">
       <h1>Конец игры</h1>
       <div className="audiocall-result">
         <div className="audiocall-result-col">
-          <div className="audiocall-result-row audiocall-true">Правильно:{props.results.correct}</div>
+          <div className="audiocall-result-row audiocall-true" style={{ fontWeight: 700 }}>{`Правильно: ${props.results.correct}`}</div>
           {props.results.correctWords.map((el) => {
             return (
               <div className="audiocall-result-row audiocall-true" key={el.id}>
@@ -22,7 +22,7 @@ export const EndGame: React.FC<iProps> = (props: iProps) => {
           })}
         </div>
         <div>
-          <div className="audiocall-result-row audiocall-false">Неправильно:{props.results.incorrect}</div>
+          <div className="audiocall-result-row audiocall-false" style={{ fontWeight: 700 }}>{`Неправильно: ${props.results.incorrect}`}</div>
           {props.results.incorrectWords.map((el) => {
             return (
               <div className="audiocall-result-row audiocall-false" key={el.id}>
@@ -32,7 +32,7 @@ export const EndGame: React.FC<iProps> = (props: iProps) => {
           })}
         </div>
       </div>
-      <Button onClick={repeatGame}>Повторить игру</Button>
+      {/*<Button onClick={repeatGame}>Повторить игру</Button>*/}
     </div>
   );
 };
--- src/components/SprintGame/EndGame.tsx
+++ src/components/SprintGame/EndGame.tsx
@@ -13,7 +13,7 @@ export const EndGame: React.FC<iProps> = (props: iProps) => {
       <h4>Набранные баллы: {score}</h4>
       <div className="audiocall-result">
         <div className="audiocall-result-col">
-          <div className="audiocall-result-row audiocall-true">Правильно:{props.results.correctWords.length}</div>
+          <div className="audiocall-result-row audiocall-true" style={{ fontWeight: 700 }}>Правильно: {props.results.correctWords.length}</div>
           {props.results.correctWords.map((el, index) => {
             return (
               <div className="audiocall-result-row audiocall-true" key={el.id}>
@@ -23,7 +23,7 @@ export const EndGame: React.FC<iProps> = (props: iProps) => {
           })}
         </div>
         <div>
-          <div className="audiocall-result-row audiocall-false">Неправильно:{props.results.incorrectWords.length}</div>
+          <div className="audiocall-result-row audiocall-false" style={{ fontWeight: 700 }}>Неправильно: {props.results.incorrectWords.length}</div>
           {props.results.incorrectWords.map((el, index) => {
             return (
               <div className="audiocall-result-row audiocall-false" key={el.id}>
