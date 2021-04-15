6e869c0f22b60f5e7c167d8ad029ec61e7781f91
Dima Kukhta <dima.kukhta.2001@mail.ru>
2021-04-15 17:38:58 +0300

    fix: clean versionr

--- src/components/Statistic/ShortStatistic.tsx
+++ src/components/Statistic/ShortStatistic.tsx
@@ -26,9 +26,9 @@ export const ShortStatistic: React.FC = () => {
     checkResult();
   }, [optional]);
 
-  /*if (!results) {
+  if (!results) {
     return <h1>Данных нету</h1>;
-  }*/
+  }
   return (
     <div className="dictionary-container">
       <h1 className="dictionary-header" style={{ color: '#FC983A' }}>
